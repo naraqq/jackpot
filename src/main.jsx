e5960f2834e4d600638494ab9f228f5fa6403423
narmandakh.g <narmandakh.g@ddishtv.mn>
2023-09-22 18:00:54 +0800

    bug gix

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.css";
-import "./fonts/MAKBANGKOK.TTF";
+import "./fonts/MAKBANGKOK.ttf";
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
