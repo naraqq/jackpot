722dcc493f21de253b3c74eaef77e7dfbff5dd0e
narmandakh.g <narmandakh.g@ddishtv.mn>
2023-09-22 18:13:56 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.css";
-import "./fonts/MAKBANGKOK.ttf";
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
