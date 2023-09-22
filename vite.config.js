fa6ec005503ba8d881710c626f584417f18a4ab3
narmandakh.g <narmandakh.g@ddishtv.mn>
2023-09-22 20:14:36 +0800

    static

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      $fonts: resolve("./public/fonts"),
+      $fonts: resolve("./static/fonts"),
     },
   },
 });
